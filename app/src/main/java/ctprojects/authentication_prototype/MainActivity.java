7b0cd7d83f41e6e72fa8e71ece09f44f0b1483d1
christophe touret <christophe.touret1@gmail.com>
2015-02-12 19:38:12 -0500

    prototype

---
+++ app/src/main/java/ctprojects/authentication_prototype/MainActivity.java
@@ -0,0 +1,194 @@
+package ctprojects.authentication_prototype;
+
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Pair;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
+
+import ctprojects.authentication.backend.myApi.MyApi;
+
+
+public class MainActivity extends ActionBarActivity {
+    private static MyApi myApiService = null;
+    private static final String URL_SERVEUR = "https://lyfeboat-backend.appspot.com/";
+    private static final String WEB_CLIENT_ID = "1004672689106-nld1kb02ik6tg8n5gp3viujfg7ea2vu7.apps.googleusercontent.com";
+    private static final int REQUEST_ACCOUNT_PICKER = 2;
+    private SharedPreferences settings;
+    private String accountName;
+    private GoogleAccountCredential credential;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        settings = getSharedPreferences("Lyfeboat", 0);
+        credential = GoogleAccountCredential.usingAudience(MainActivity.this, "server:client_id:" + WEB_CLIENT_ID);
+        setAccountName(settings.getString("ACCOUNT_NAME", null));
+        if (credential.getSelectedAccountName() != null) {
+// Already signed in, begin app!
+            Toast.makeText(getBaseContext(), "Logged in with : " + credential.getSelectedAccountName(), Toast.LENGTH_SHORT).show();
+//Toast.makeText(getBaseContext(), GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext()),Toast.LENGTH_SHORT).show();
+        } else {
+// Not signed in, show login window or request an account.
+            chooseAccount();
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void setAccountName(String accountName) {
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("ACCOUNT_NAME", accountName);
+        editor.commit();
+        credential.setSelectedAccountName(accountName);
+        this.accountName = accountName;
+    }
+
+    void chooseAccount() {
+        startActivityForResult(credential.newChooseAccountIntent(),
+                REQUEST_ACCOUNT_PICKER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_ACCOUNT_PICKER:
+                if (data != null && data.getExtras() != null) {
+                    String accountName =
+                            data.getExtras().getString(
+                                    AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        setAccountName(accountName);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString("ACCOUNT_NAME", accountName);
+                        editor.commit();
+                    }
+                }
+                break;
+        }
+    }
+    private class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if(myApiService == null) {  // Only do this once
+
+
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+//                        .setRootUrl(URL_SERVEUR+"_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                return myApiService.sayHi(name).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+    private class EndpointsAsyncTaskAuth extends AsyncTask<Pair<Context, String>, Void, String> {
+
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+//            if(myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), credential  )                   // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl(URL_SERVEUR+"_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+//            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                return myApiService.sayHiAuth(name).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ backend/src/main/java/ctprojects/authentication/backend/ApiKeysAndIds.java
@@ -0,0 +1,20 @@
+package ctprojects.authentication.backend;
+
+/**
+ * Reference
+ * https://github.com/GoogleCloudPlatform/solutions-mobile-shopping-assistant-backend-java/blob/master/src/com/google/sample/mobileassistant/ApiKeysAndIds.java
+ */
+public class ApiKeysAndIds {
+
+    static final String GCM_API_KEY = "!!! ENTER YOUR GCM API KEY HERE !!!";
+
+    static final String ANDROID_CLIENT_ID = "1004672689106-f5rra2b4jnp8g45vbairom6ght3kmf8h.apps.googleusercontent.com";
+    static final String ANDROID_CLIENT_ID_TESTER = "1004672689106-nld1kb02ik6tg8n5gp3viujfg7ea2vu7.apps.googleusercontent.com";
+    static final String IOS_CLIENT_ID = "1004672689106-58fmcqbfg9v8ekfjo0el47p5r4fralvt.apps.googleusercontent.com";
+    static final String WEB_CLIENT_ID ="1004672689106-nld1kb02ik6tg8n5gp3viujfg7ea2vu7.apps.googleusercontent.com";
+    static final String AUDIENCE_ID = WEB_CLIENT_ID;
+
+    static final String API_OWNER = "lyfeboat.com";//Original -> google.com
+    static final String API_PACKAGE_PATH = "lyfeboat.com";//Original -> sample.mobileassistant
+    static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
